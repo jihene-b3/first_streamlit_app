c7a443e84deb720c11c042a589a863e801a2a02b
jihene-b3 <78120230+jihene-b3@users.noreply.github.com>
2023-04-21 21:45:48 +0200

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Bolled Free-Range Egg")
+streamlit.header("🥣Breakfast Menu")
+streamlit.text("🥗Omega 3 & Blueberry Oatmeal")
+streamlit.text("🐔Kale, Spinach & Rocket Smoothie")
+streamlit.text("🥑🍞Hard-Bolled Free-Range Egg")
