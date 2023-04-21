3efacaa2bc435480adcac159fe6b78818e029c58
jihene-b3 <78120230+jihene-b3@users.noreply.github.com>
2023-04-21 21:47:20 +0200

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -7,3 +7,5 @@ streamlit.header("🥣Breakfast Menu")
 streamlit.text("🥗Omega 3 & Blueberry Oatmeal")
 streamlit.text("🐔Kale, Spinach & Rocket Smoothie")
 streamlit.text("🥑🍞Hard-Bolled Free-Range Egg")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
